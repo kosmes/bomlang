0fc21a7446aec3017c5d2835572e79000ca332d7
kosmes <kosmes@gmail.com>
2019-03-03 14:16:51 +0900

    feat: Add common header

--- srcs/corelib/common.h
+++ srcs/corelib/common.h
@@ -3,4 +3,16 @@
 
 #include "platform.h"
 
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <memory.h>
+
 #endif /* ifndef COMMON_H */
