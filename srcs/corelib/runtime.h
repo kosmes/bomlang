8b0754d6b8832db32255d7db69974191b0a15947
kosmes <kosmes9779@gmail.com>
2019-03-17 21:24:12 +0900

    feat: Add runtime header to contain data what need in runtime

---
+++ srcs/corelib/runtime.h
@@ -0,0 +1,38 @@
+//
+// Created by jiney on 2019-03-17.
+//
+
+#ifndef BOM_OPCODE_H
+#define BOM_OPCODE_H
+
+enum TypeCode {
+    TYPE_INT,
+    TYPE_DOUBLE,
+};
+
+enum OpCode {
+    OP_HALT,
+
+    OP_CONST = 0x10 ,
+    OP_STORE,
+    OP_LOAD,
+
+    OP_ADD,
+    OP_SUB,
+    OP_MUL,
+    OP_DIV,
+
+    /**
+     * @brief   스택의 꼭대기서부터 2개를 비교한 후 결과를 스택에 집어넣는다.
+     * @code    OP_CAST '비교할 타입'
+     */
+    OP_CMP,
+
+    /**
+     * @brief   스택의 꼭대기의 값을 다른 형으로 변환한다.
+     * @code    OP_CAST '바꿀 타입'
+     */
+    OP_CAST,
+};
+
+#endif //BOM_OPCODE_H
