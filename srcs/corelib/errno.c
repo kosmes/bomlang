52653234d4116c94182b868c0dfba29cf61445ee
kosmes <kosmes9779@gmail.com>
2019-03-17 10:28:59 +0900

    fix: Update errno to move list to src for fix dupl decl

--- srcs/corelib/errno.c
+++ srcs/corelib/errno.c
@@ -4,6 +4,13 @@
 
 #include "errno.h"
 
+const wchar_t *errstr[] = {
+        L"구문 오류.",
+        L"식이 없습니다.",
+        L"';'가 필요합니다.",
+        L"'(' ')'가 올바르게 연결되지 않았습니다.",
+};
+
 void error(enum errco err, unsigned int line) {
     wprintf(L"오류 [ %d 줄 ] %ls", line, errstr[err]);
 }
