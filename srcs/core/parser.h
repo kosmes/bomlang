0c9f4fa4ae7aeb1a80fa7b6e58a0c3f42f0bf57b
kosmes <kosmes9779@gmail.com>
2019-03-20 12:21:19 +0900

    docs: Update parser to add document

--- srcs/core/parser.h
+++ srcs/core/parser.h
@@ -9,6 +9,11 @@
 #include "node.h"
 #include "token.h"
 
+/**
+ * @brief   주어진 토큰을 분석하여 ast 노드로 변환한다
+ * @param   tokens  분석한 토큰들
+ * @return  변환된 노드
+ */
 DLL_EXPORT node_t *do_parse(token_t *tokens);
 
 #endif //BOM_PARSER_H
