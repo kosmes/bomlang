5f62280782ab23165aa3f516226bfdcfae92cf8c
kosmes <kosmes9779@gmail.com>
2019-03-20 11:49:09 +0900

    docs: Update token to add document

--- srcs/core/token.h
+++ srcs/core/token.h
@@ -7,9 +7,19 @@
 
 #include "common.h"
 
+/**
+ * @brief   토큰의 종류들을 모아놓은 열거형
+ */
 typedef enum TOKEN_TYPES TOKEN_TYPES;
+
+/**
+ * @brief   토큰종류와 속성값으로 구성되는 데이터, 각 패턴에 부합하는 어휘 항목을 가지고 있는다
+ */
 typedef struct token token_t;
 
+/**
+ * @brief   토큰의 종류들을 모아놓은 열거형
+ */
 enum TOKEN_TYPES {
     TokenNone,
     TokenComma,
@@ -45,20 +55,21 @@ struct token {
         double f64;
     };
 
+    /*  @brief  토큰의 문자열 값 */
     wchar_t *str;
 };
 
 /**
- * 토큰의 종류와 라인 값으로 토큰을 만든다
- * @param type  토큰의 종류
- * @param line  토큰의 라인
+ * @brief   토큰의 종류와 라인 값으로 토큰을 만든다
+ * @param   type    토큰의 종류
+ * @param   line    토큰의 라인
  * @return  초기화된 토큰
  */
 DLL_EXPORT token_t create_token(TOKEN_TYPES type, unsigned int line);
 
 /**
- * 토큰을 파괴한다
- * @param token[in] 파괴할 토큰
+ * @brief   토큰을 파괴한다
+ * @param   token[in]   파괴할 토큰
  */
 DLL_EXPORT void destroy_token(token_t *token);
 
