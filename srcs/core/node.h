ba33439d671447cb0b8e4481c513927d8b0103d8
kosmes <kosmes9779@gmail.com>
2019-03-20 11:46:35 +0900

    docs: Update node to add document

--- srcs/core/node.h
+++ srcs/core/node.h
@@ -9,31 +9,56 @@
 
 #include "token.h"
 
+/**
+ * @brief   노드 종류들을 모아놓은 열거형
+ */
 typedef enum NODE_TYPES NODE_TYPES;
+
+/**
+ * @brief   트리형으로 구성된 AST 노드
+ */
 typedef struct node node_t;
 
+/**
+ * @brief   노드 종류들을 모아놓은 열거형
+ */
 enum NODE_TYPES {
-    NodeIntegerConstant,
-    NodeFPConstant,
+    NodeIntegerConstant,    /* < 정수 상수 */
+    NodeFPConstant,         /* < 실수 상수 */
 
-    NodeBinOp,
-    NodeUnaryOp,
-    NodeAssignOp,
+    NodeBinOp,              /* < 2항 연산식 */
+    NodeUnaryOp,            /* < 단항 연산 */
+    NodeAssignOp,           /* < 대입식 */
 
-    NodeVar,
+    NodeVar,                /* < 변수 참조식 */
 
     // stmt
-    NodeCompound,
-    NodeEmpty,
+    NodeCompound,           /* < 복문 */
+    NodeEmpty,              /* < 빈 식*/
 };
 
+/**
+ * @brief   트리형으로 구성된 AST 노드
+ */
 struct node {
-    NODE_TYPES type;
-    token_t token;
-    node_t **child;
+    NODE_TYPES type;    /* < 노드의 종류 */
+    token_t token;      /* < 노드의 토큰 */
+    node_t **child;     /* < 노드의 자식들 */
 };
 
+/**
+ * @brief   주어진 정보로 노드를 생성한다
+ * @param   type    노드의 종류
+ * @param   token   노드의 토큰
+ * @param   child   노드의 자식들
+ * @return  생성된 노드
+ */
 DLL_EXPORT node_t *create_node(NODE_TYPES type, token_t token, node_t **child);
+
+/**
+ * @brief   노드의 자식들을 삭제하고 파괴한다
+ * @param   node    파괴할 노드
+ */
 DLL_EXPORT void destroy_node(node_t *node);
 
 #endif //BOM_NODE_H
