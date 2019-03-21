115370bc80fd4a3797ce4199ad4dcfa5da97872a
kosmes <kosmes9779@gmail.com>
2019-03-21 14:24:27 +0900

    refactor: Update to move print node method to node file

--- srcs/core/node.h
+++ srcs/core/node.h
@@ -65,4 +65,6 @@ DLL_EXPORT node_t *create_node(NODE_TYPES type, token_t token, node_t **child);
  */
 DLL_EXPORT void destroy_node(node_t *node);
 
+DLL_EXPORT void print_node(node_t *node, int indent);
+
 #endif //BOM_NODE_H
