a4b6c40f1d07645183f540339d69e702ddc009a8
kosmes <kosmes9779@gmail.com>
2019-03-23 19:23:25 +0900

    feat: Add symbol

---
+++ srcs/core/symbol/symbol.h
@@ -0,0 +1,26 @@
+﻿//
+// Created by jiney on 2019-03-23.
+//
+
+#ifndef BOM_SYMBOL_H
+#define BOM_SYMBOL_H
+
+#include "common.h"
+
+typedef struct symbol Symbol;
+
+enum SYMBOL_TYPE {
+    SYMBOL_TYPE_BUILTIN,
+    SYMBOL_TYPE_VAR,
+    SYMBOL_TYPE_FUNC,
+};
+
+struct symbol {
+    unsigned char type;
+    u16char *name;
+    Symbol *base;
+};
+
+DLL_EXPORT Symbol *SymbolCreate(unsigned char type, const u16char *name, Symbol *base);
+
+#endif //BOM_SYMBOL_H
